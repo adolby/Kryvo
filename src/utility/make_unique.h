f321122db0e59defbb1e08fef0ed1a9f401f86d3
adolby <andrewdolby@gmail.com>
2014-09-03 23:08:39 -0400

    Added include guard to make_unique.h
    Added a header guard to make_unique.h to protect against multiple
    inclusion.

--- src/utility/make_unique.h
+++ src/utility/make_unique.h
@@ -18,6 +18,9 @@
  * Contact : andrewdolby@gmail.com
  */
 
+#ifndef KRYVOS_UTILITY_MAKE_UNIQUE_H_
+#define KRYVOS_UTILITY_MAKE_UNIQUE_H_
+
 #include <memory>
 
 template<typename T, typename... Args>
@@ -25,3 +28,5 @@ std::unique_ptr<T> make_unique(Args&&... args)
 {
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
+
+#endif // KRYVOS_UTILITY_MAKE_UNIQUE_H_
