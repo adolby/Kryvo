20dfe3178efd63e1446aa98bb70e49fdbce61860
adolby <andrewdolby@gmail.com>
2015-04-04 18:00:11 -0400

    Update license headers
    Update license headers in most project files.

--- src/cryptography/botan/android/android_to_string.h
+++ src/cryptography/botan/android/android_to_string.h
@@ -1,5 +1,27 @@
-#ifndef KRYVOS_CRYPTOGRAPHY_ANDROID_TO_STRING_H__
-#define KRYVOS_CRYPTOGRAPHY_ANDROID_TO_STRING_H__
+/**
+ * Kryvos - Encrypts and decrypts files.
+ * Copyright (C) 2014, 2015 Andrew Dolby
+ *
+ * This file is part of Kryvos.
+ *
+ * Kryvos is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kryvos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kryvos.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact : andrewdolby@gmail.com
+ */
+
+#ifndef KRYVOS_CRYPTOGRAPHY_ANDROID_TO_STRING_H_
+#define KRYVOS_CRYPTOGRAPHY_ANDROID_TO_STRING_H_
 
 #include <sstream>
 
@@ -15,4 +37,4 @@ std::string to_string(T value)
 
 }
 
-#endif // KRYVOS_CRYPTOGRAPHY_ANDROID_TO_STRING_H__
+#endif // KRYVOS_CRYPTOGRAPHY_ANDROID_TO_STRING_H_
--- src/gui/HeaderFrame.cpp
+++ src/gui/HeaderFrame.cpp
@@ -1,19 +1,21 @@
 /**
- * Kryvos File Encryptor - Encrypts and decrypts files.
- * Copyright (C) 2014 Andrew Dolby
+ * Kryvos - Encrypts and decrypts files.
+ * Copyright (C) 2014, 2015 Andrew Dolby
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of Kryvos.
+ *
+ * Kryvos is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Kryvos is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Kryvos.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Contact : andrewdolby@gmail.com
  */
