2ab109a3cdfb71e820ce0868c5e0c1fcacad8661
Andrew Dolby <andrewdolby@gmail.com>
2015-04-20 19:31:31 -0400

    Fix Android build
    Use boost::format to replace sprintf that's missing on Android NDK.

--- src/cryptography/botan/android/android_to_string.h
+++ src/cryptography/botan/android/android_to_string.h
@@ -31,9 +31,9 @@ namespace std {
 template <typename T>
 std::string to_string(T value)
 {
-    std::ostringstream os;
-    os << value;
-    return os.str();
+  std::ostringstream os;
+  os << value;
+  return os.str();
 }
 
 }
