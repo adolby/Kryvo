0240b13b163fb008ec3ff0214dc7ccd9c04c0756
Andrew Dolby <andrewdolby@gmail.com>
2015-04-27 10:13:03 -0400

    Use make_unique in pimpl helper

--- src/utility/pimpl_impl.h
+++ src/utility/pimpl_impl.h
@@ -1,15 +1,16 @@
 #ifndef PIMPL_IMPL_H
 #define PIMPL_IMPL_H
 
+#include "make_unique.h"
 #include <utility>
 
 template<typename T>
-pimpl<T>::pimpl() : m{ new T{} } { }
+pimpl<T>::pimpl() : m{ make_unique<T>() } { }
 
 template<typename T>
 template<typename ...Args>
 pimpl<T>::pimpl( Args&& ...args )
-  : m{ new T{ std::forward<Args>(args)... } } { }
+  : m{ make_unique<T>(std::forward<Args>(args)...) } { }
 
 template<typename T>
 pimpl<T>::~pimpl() { }
