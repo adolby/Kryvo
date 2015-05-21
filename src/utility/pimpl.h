0ca2be37b6ca0beda416a3404a310d66d1461caf
Andrew Dolby <andrewdolby@gmail.com>
2015-05-21 18:57:23 -0400

    Add license header and include guard to pimpl

--- src/utility/pimpl.h
+++ src/utility/pimpl.h
@@ -1,5 +1,27 @@
-#ifndef PIMPL_H_H
-#define PIMPL_H_H
+/**
+ * Kryvos - Encrypts and decrypts files.
+ * Copyright (C) 2014, 2015 Andrew Dolby
+ *
+ * This file is part of Kryvos.
+ *
+ * Kryvos is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kryvos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kryvos.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact : andrewdolby@gmail.com
+ */
+
+#ifndef KRYVOS_UTILITY_PIMPL_H_
+#define KRYVOS_UTILITY_PIMPL_H_
 
 #include <memory>
 
@@ -18,4 +40,4 @@ class pimpl {
   std::unique_ptr<T> m;
 };
 
-#endif
+#endif // KRYVOS_UTILITY_PIMPL_H_
