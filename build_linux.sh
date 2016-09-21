384e4c05228b8354218ff297ef5a452153160061
Andrew Dolby <andrewdolby@gmail.com>
2016-09-21 17:54:37 -0400

    Try building without optimized qmake

--- build_linux.sh
+++ build_linux.sh
@@ -35,7 +35,7 @@ tar xf qt-everywhere-opensource-src-5.7.0.tar
 
 cd qt-everywhere-opensource-src-5.7.0
 sudo chmod +x configure
-./configure -platform linux-clang -opensource -confirm-license -release -skip qtwebengine -nomake examples -optimized-qmake
+./configure -platform linux-clang -opensource -confirm-license -release -skip qtwebengine -nomake examples
 
 make
 make install
