dc17eff5c87808863e96e9e9af7d4fd212636a84
Andrew Dolby <andrewdolby@gmail.com>
2016-09-22 00:14:56 -0400

    Update Linux build

--- build_linux.sh
+++ build_linux.sh
@@ -4,20 +4,20 @@ set -o errexit -o nounset
 
 # Update platform
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libfontconfig1 -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libfontconfig1-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libfreetype6-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libx11-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libxext-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libxfixes-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libxi-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libxrender-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libxcb1-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libx11-xcb-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libxcb-glx0-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install mesa-common-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libglu1-mesa-dev -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libfontconfig1 -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libfontconfig1-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libfreetype6-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libx11-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libxext-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libxfixes-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libxi-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libxrender-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libxcb1-dev -y -qq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libx11-xcb-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libxcb-glx0-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install mesa-common-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libglu1-mesa-dev -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 
 # Capture build directory
 project=$(pwd)
@@ -31,8 +31,8 @@ tar xf qt-everywhere-opensource-src-5.7.0.tar
 cd qt-everywhere-opensource-src-5.7.0
 sudo chmod +x configure
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 100
-${CXX}
-${CC}
+echo ${CXX}
+echo ${CC}
 ./configure -platform linux-g++-64 -opensource -confirm-license -release -c++std c++14 -shared -largefile -no-qml-debug -qt-libpng -qt-libjpeg -qt-doubleconversion -qt-harfbuzz -openssl -qt-pcre -skip qtwebengine -nomake examples
 
 make
@@ -58,7 +58,6 @@ cp "../../../../../README.md" "README.md"
 cp "../../../../../LICENSE" "LICENSE"
 cp "../../../../../Botan License" "Botan License"
 cp "../../../../../Qt License" "Qt License"
-ls
 7z a kryvos_${TRAVIS_TAG}_linux_x86_64.zip "Kryvos" "Release Notes" "README.md" "LICENSE" "Botan License" "Qt License"
 
 exit 0
