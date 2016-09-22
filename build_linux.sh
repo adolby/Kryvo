37be8d9e5d0e00296cbe95b1326bd65c78224c18
Andrew Dolby <andrewdolby@gmail.com>
2016-09-22 19:13:29 -0400

    Attempt to fix Linux build script

--- build_linux.sh
+++ build_linux.sh
@@ -72,16 +72,18 @@ cp "../../../../../README.md" "Kryvos/README.md"
 cp "../../../../../LICENSE" "Kryvos/LICENSE"
 cp "../../../../../Botan License" "Kryvos/Botan License"
 cp "../../../../../Qt License" "Kryvos/Qt License"
-
-ls
+rm -rf moc
+rm -rf obj
+rm -rf qrc
 
 echo "Packaging portable archive..."
-cp -R Kryvos/ "../../../../../installer/linux/packages/com.kryvosproject.kryvos/data/"
+cd Kryvos
+cp -R * "../../../../../installer/linux/packages/com.kryvosproject.kryvos/data/"
 7z a kryvos_${TRAVIS_TAG}_linux_x86_64_portable.zip "Kryvos" "libQt5Core.so" "libQt5Gui.so" "libQt5Svg.so" "libQt5Widgets.so" "Release Notes" "README.md" "LICENSE" "Botan License" "Qt License"
 
 echo "Building installer..."
 cd "../../../../../installer/linux/"
-/usr/local/QtIFW2.0.3/bin/binarycreator --offline-only -c config\config.xml -p packages kryvos_${TRAVIS_TAG}_linux_x86_64_installer
+/usr/local/QtIFW2.0.3/bin/binarycreator --offline-only -c config/config.xml -p packages kryvos_${TRAVIS_TAG}_linux_x86_64_installer
 
 echo "Done!"
 
