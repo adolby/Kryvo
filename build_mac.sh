f819c824b1d98daa9e12e3b86cf6bfbf7bf42b2d
Andrew Dolby <andrewdolby@gmail.com>
2016-09-22 17:05:20 -0400

    Add more user trace lines

--- build_mac.sh
+++ build_mac.sh
@@ -3,6 +3,7 @@
 set -o errexit -o nounset
 
 # Update platform
+echo "Updating platform..."
 brew update
 brew install qt5
 brew install p7zip
@@ -10,11 +11,13 @@ npm install -g appdmg
 chmod -R 755 /usr/local/opt/qt5/*
 
 # Build
+echo "Building Kryvos..."
 cd src
 /usr/local/opt/qt5/bin/qmake -config release
 make -j2
 
 # Run tests
+echo "Running tests..."
 cd tests
 /usr/local/opt/qt5/bin/qmake -config release
 make
@@ -22,15 +25,23 @@ cd ../../build/macx/clang/x86_64/release/test/
 open CryptoTests.app
 
 # Package
+echo "Packaging..."
 cd ..
+
+echo "Creating dmg archive..."
 /usr/local/opt/qt5/bin/macdeployqt Kryvos.app -dmg
 mv Kryvos.dmg "Kryvos_${TRAVIS_TAG}.dmg"
 # appdmg json-path Kryvos_${TRAVIS_TAG}.dmg
+
 cp "../../../../../Release Notes" "Release Notes"
 cp "../../../../../README.md" "README.md"
 cp "../../../../../LICENSE" "LICENSE"
 cp "../../../../../Botan License" "Botan License"
 cp "../../../../../Qt License" "Qt License"
+
+echo "Packaging zip archive..."
 7z a kryvos_${TRAVIS_TAG}_macos.zip "Kryvos_${TRAVIS_TAG}.dmg" "Release Notes" "README.md" "LICENSE" "Botan License" "Qt License"
+
+echo "Done!"
 
 exit 0
