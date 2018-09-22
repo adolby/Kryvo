16fe6e32ae47c9b16a7d08d73a2e53b64b9e73f0
Andrew Dolby <andrewdolby@gmail.com>
2018-09-21 22:35:03 -0400

    Update Thread implementation

--- src/app/utility/Thread.hpp
+++ src/app/utility/Thread.hpp
@@ -2,17 +2,66 @@
 #define KRYVO_UTILITY_THREAD_HPP_
 
 #include <QThread>
+#include <QAbstractEventDispatcher>
+#include <QVariant>
+#include <QEvent>
+#include <QCoreApplication>
+#include <QTimer>
 #include <QObject>
 
 namespace Kryvo {
 
-class Thread : public QThread {
-  Q_OBJECT
-  using QThread::run; // This is a final class
+class Thread final : public QThread {
+   Q_OBJECT
+   void run() override {
+      connect(QAbstractEventDispatcher::instance(this),
+              &QAbstractEventDispatcher::aboutToBlock,
+              this, &Thread::aboutToBlock);
+      QThread::run();
+   }
+   QAtomicInt inDestructor;
+public:
+   using QThread::QThread;
+   /// Take an object and prevent timer resource leaks when the object is about
+   /// to become threadless.
+   void takeObject(QObject *obj) {
+      // Work around to prevent
+      // QBasicTimer::stop: Failed. Possibly trying to stop from a different thread
+      static constexpr char kRegistered[] = "__ThreadRegistered";
+      static constexpr char kMoved[] = "__Moved";
+      if (!obj->property(kRegistered).isValid()) {
+         QObject::connect(this, &Thread::finished, obj, [this, obj]{
+            if (!inDestructor.load() || obj->thread() != this)
+               return;
+            // The object is about to become threadless
+            Q_ASSERT(obj->thread() == QThread::currentThread());
+            obj->setProperty(kMoved, true);
+            obj->moveToThread(this->thread());
+         }, Qt::DirectConnection);
+         QObject::connect(this, &QObject::destroyed, obj, [obj]{
+            if (!obj->thread()) {
+               obj->moveToThread(QThread::currentThread());
+               obj->setProperty(kRegistered, {});
+            }
+            else if (obj->thread() == QThread::currentThread() && obj->property(kMoved).isValid()) {
+               obj->setProperty(kMoved, {});
+               QCoreApplication::sendPostedEvents(obj, QEvent::MetaCall);
+            }
+            else if (obj->thread()->eventDispatcher())
+               QTimer::singleShot(0, obj, [obj]{ obj->setProperty(kRegistered, {}); });
+         }, Qt::DirectConnection);
 
- public:
-  explicit Thread(QObject* parent = nullptr);
-  ~Thread() final;
+         obj->setProperty(kRegistered, true);
+      }
+      obj->moveToThread(this);
+   }
+   ~Thread() override {
+      inDestructor.store(1);
+      requestInterruption();
+      quit();
+      wait();
+   }
+   Q_SIGNAL void aboutToBlock();
 };
 
 } // namespace Kryvo
