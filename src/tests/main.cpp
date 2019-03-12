3275c7c83fff2146318b6cbfbbfc86ccced268b6
Andrew Dolby <andrewdolby@gmail.com>
2019-03-12 13:23:38 -0400

    Clean up spacing

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -5,10 +5,10 @@
 #include <QtTest>
 
 int main(int argc, char* argv[]) {
-    QCoreApplication a(argc, argv);
+  QCoreApplication a(argc, argv);
 
-    QTEST_SET_MAIN_SOURCE_PATH
-    const int result = Catch::Session().run(argc, argv);
+  QTEST_SET_MAIN_SOURCE_PATH
+  const int result = Catch::Session().run(argc, argv);
 
-    return (result < 0xFF ? result : 0xFF);
+  return (result < 0xFF ? result : 0xFF);
 }
