961c060ffe8a473f756ea610474073b46952c3f3
Andrew Dolby <andrewdolby@gmail.com>
2019-03-22 12:17:27 -0400

    Use 7z's in place extract mode instead of full paths extract mode

--- build_windows_msvc.cmd
+++ build_windows_msvc.cmd
@@ -13,7 +13,7 @@ nmake
 echo Copying test data...
 cd %project_dir%\build\windows\msvc\x86_64\release\test\
 cp %project_dir%\src\tests\data\test-data.zip test-data.zip
-7z x test-data.zip
+7z e test-data.zip
 
 echo Running tests...
 windeployqt tests.exe
