0e7bae1df8b76a252bac31e4621b2503919aca5f
Andrew Dolby <andrewdolby@gmail.com>
2023-03-14 16:19:06 -0400

    Set tag and Qt tool path

--- build_windows_msvc.cmd
+++ build_windows_msvc.cmd
@@ -1,10 +1,12 @@
 echo on
 
+echo "Setting up environment..."
+
 SET project_dir="%SOURCE_DIR%"
 SET qt_path="%Qt6_DIR%"
-
-echo "Setting up environment..."
-set PATH=%qt_path%\bin\;C:\Qt\QtIFW-3.0.1\bin\;%PATH%
+SET qt_tools=%IQTA_TOOLS%
+SET tag_name="%TAG_NAME%"
+SET PATH=%qt_path%\bin\;%qt_tools%\QtInstallerFramework\4.5\bin;%PATH%
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
 
 cd %project_dir%
@@ -53,7 +55,7 @@ copy "%project_dir%\README.md" "Kryvo\README.md"
 copy "%project_dir%\LICENSE" "Kryvo\LICENSE.txt"
 copy "%project_dir%\Botan License" "Kryvo\Botan License.txt"
 copy "%project_dir%\Qt License" "Kryvo\Qt License.txt"
-mkdir "%project_dir%\Kryvo\themes\"
+mkdir "%project_dir%\build\windows\msvc\x86_64\release\Kryvo\themes"
 copy "%project_dir%\resources\stylesheets\kryvo.qss" "Kryvo\themes\kryvo.qss"
 
 echo "Copying files for installer..."
@@ -61,8 +63,8 @@ mkdir "%project_dir%\installer\windows\x86_64\packages\app.kryvo\data\"
 robocopy Kryvo\ "%project_dir%\installer\windows\x86_64\packages\app.kryvo\data" /E
 
 echo "Packaging portable archive..."
-7z a -aoa kryvo_%TAG_NAME%_windows_x86_64_portable.zip Kryvo
+7z a -aoa kryvo_%tag_name%_windows_x86_64_portable.zip Kryvo
 
 echo "Creating installer..."
 cd "%project_dir%\installer\windows\x86_64\"
-binarycreator --offline-only -c config\config.xml -p packages kryvo_%TAG_NAME%_windows_x86_64_installer.exe
+binarycreator --offline-only -c config\config.xml -p packages kryvo_%tag_name%_windows_x86_64_installer.exe
