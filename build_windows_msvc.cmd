3d4290beae772c44e803c9c1fd842929a4e839a6
Andrew Dolby <andrewdolby@gmail.com>
2023-03-14 15:01:46 -0400

    Try something

--- build_windows_msvc.cmd
+++ build_windows_msvc.cmd
@@ -4,7 +4,7 @@ SET project_dir="%cd%"
 
 echo "Setting up environment..."
 set PATH=%QTPATH%\bin\;C:\Qt\QtIFW-3.0.1\bin\;%PATH%
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
 
 echo "Building Kryvo..."
 %QTPATH%\bin\qmake.exe -spec win32-msvc CONFIG+=x86_64 CONFIG-=debug CONFIG+=release
